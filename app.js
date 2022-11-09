4de1f81863c4e877c1b27cc0113786ae57e67f45
Wadad Parker <wadadparker5@gmail.com>
2022-11-09 22:28:13 +0530

    fix: Number( )

--- app.js
+++ app.js
@@ -6,13 +6,16 @@ var nextButton=document.querySelector("#next-button");
 var displayMessage=document.querySelector("#display-message");
 var notes= document.querySelectorAll(".notes");
 
+
+
 var noteList = [2000,500,100,20,10,5,1];
 checkButton.style.display="none";
 cashGiven.style.display="none";
 cashLabel.style.display="none";
 nextButton.addEventListener("click",function visible() {
     displayMessage.style.display="none";
-    if(billAmount.value<1)
+    console.log(Number(billAmount.value));
+    if(Number(billAmount.value)<1)
     {
         displayMessageFn("Please enter valid bill amount");
     }
@@ -29,12 +32,12 @@ checkButton.addEventListener("click", function computeChange()
 {
     displayMessage.style.display="none";
     console.log("clicked");
-    if(billAmount.value>0)
+    if(Number(billAmount.value)>0)
     {
         
-    if(cashGiven.value>billAmount.value)
+    if(Number(cashGiven.value)>Number(billAmount.value))
     {
-        var finalAmount= cashGiven.value - billAmount.value;
+        var finalAmount= Number(cashGiven.value) - Number(billAmount.value);
         calculateChange(finalAmount);
     }
     else {
@@ -42,7 +45,7 @@ checkButton.addEventListener("click", function computeChange()
     }
 }
     else {
-        displayMessageFn("Please enter a positive number");
+        displayMessageFn("Please enter a positive Number");
     }
 }
 )
@@ -51,8 +54,8 @@ function calculateChange(amount) {
         for(let i=0; i<noteList.length;i++)
         {   
             console.log("Workinggg");
-            var finalNote= Math.trunc(amount/noteList[i]);
-            amount%=noteList[i];
+            var finalNote= Math.trunc(amount/Number(noteList[i]));
+            amount%=Number(noteList[i]);
             notes[i].innerText=finalNote;
         }
 
